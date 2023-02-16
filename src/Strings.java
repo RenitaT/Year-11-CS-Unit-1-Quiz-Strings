ae410e1af7027d983dc4b6106600ee3091554588
RenitaT <renitat2025@student.cis.edu.hk>
2023-02-16 10:09:45 +0800

    why

--- src/Strings.java
+++ src/Strings.java
@@ -4,23 +4,50 @@ public class Strings {
 
         // 1.
 
+        int age = 33;
+
         // 2.
+
+        String name = "T Swizzle";
 
         // 3.
 
+        System.out.println(age);
+
         // 4.
+
+        System.out.println(name);
 
         // 5.
 
+        String description = "My name is "+name+" and I am "+age+" years old";
+
         // 6.
+
+        System.out.println(description);
 
         // 7.
 
+        System.out.println(description.length());
+
         // 8.
+
+        String nameSubDescription = description.substring(11,20);
+
+        System.out.println(nameSubDescription);
+
 
         // 9.
 
+        int ageSubDescription = Integer.parseInt(description.substring(30,32));
+
+        System.out.println(ageSubDescription);
+
         // 10.
+
+        String subDescription = "My name is "+nameSubDescription+" and I am "+ageSubDesciption+" years old";
+
+        System.out.println(subDescription);
 
     }
 
